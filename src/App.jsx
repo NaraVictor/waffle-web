70db329fb2108506e24e70f258b3676922a4de94
Nara Victor <naravictor@live.com>
2021-03-28 23:29:08 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h3>Waffle Web Client</h3>
+			</header>
+		</div>
+	);
+}
+
+export default App;
