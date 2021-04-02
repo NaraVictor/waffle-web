27ce91d59e9bee9a8280c5eb6439f6070daf249b
Nara Victor <naravictor@live.com>
2021-04-02 22:51:41 +0000

    colors, text, button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import "./App.css";
+import "../src/static/css/styles.css";
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<h3>Waffle Web Client</h3>
+				<button className="button button__primary">Follow</button>
 			</header>
 		</div>
 	);
